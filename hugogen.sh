b304e14391e98b8e6d402f3fdf5ebae9d2eb0201
Dreg <dreg@fr33project.org>
2023-11-25 04:45:02 +0100

    commit

--- hugogen.sh
+++ hugogen.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 set -x
-hugo && cp docs/CNAME public/ && rm -rf docs/ && mv public/ docs/
-
+hugo && cp docs/CNAME public/ && rm -rf docs/ && mv public/ docs/ && git add . && git commit -a -m 'commit'
